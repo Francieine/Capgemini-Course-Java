3a9a9e4fcb9c1f49c2400ff9c14db0a636b9be30
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-10-30 10:13:36 -0300

    Vehicle.java

---
+++ Exercicios OOP/Exercicio02/Vehicle.java
@@ -0,0 +1,13 @@
+
+public class Vehicle {
+	
+	/*Attributes*/	
+	int passangers;
+	int fuelcap;
+	int mpg;
+	
+	/*Method/calculation/data processing*/
+	int ranger() {
+		 return mpg * fuelcap;
+	 }
+}
