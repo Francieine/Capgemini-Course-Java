a1471b5014b0251fabf49659b0e559e9ab16010c
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-12-07 14:04:51 -0300

    Pessoa.java

---
+++ Exercicios OOP/Methods/Pessoa.java
@@ -0,0 +1,22 @@
+package Modelos;
+
+public class Pessoa {
+
+	String nome;
+	int idade;
+	
+	
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public void exibirInformacoes() {
+		System.out.println("Nome: " + nome);
+		System.out.println("Nome: " + idade + "anos");
+	}
+	}
+	
+	
+	
+
