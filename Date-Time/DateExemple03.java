5b1e672f142cba4beb25c704470073d6efcb37a7
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-12-14 08:45:36 -0300

    DateExemple03.java

---
+++ Date-Time/DateExemple03.java
@@ -0,0 +1,13 @@
+import java.time.Instant;
+
+public class DateExemple01 {
+
+	public static void main(String[] args) {
+		
+		
+		Instant DataHoraGlobal = Instant.now(); // GTM or UTC
+		
+		System.out.println("Data-Hora-Global = " + DataHoraGlobal);
+		
+		}
+	}
