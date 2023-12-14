f5f30aeff3103ce90e15093f04e32fd2410bceb8
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-12-14 08:39:19 -0300

    DateExemple02.java

---
+++ Date-Time/DateExemple02.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class DateExemple02 {
+
+	public static void main(String[] args) {
+
+		LocalDateTime DataHoraLocal = LocalDateTime.now();
+    
+		System.out.println("Data-Hora = " + DataHoraLocal);
+
+	}
+
+}
